e31eb396a1e2a959455fb2932ae384b5a29e4a6f
Joachim Breitner <mail@joachim-breitner.de>
2020-03-03 17:16:09 +0100

    Random cleanup

--- impl/src/IC/HTTP/WAI.hs
+++ impl/src/IC/HTTP/WAI.hs
@@ -8,7 +8,6 @@ import Network.HTTP.Types
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Control.Monad.State
-import Data.Tuple
 import Data.Aeson as JSON
 
 import IC.Ref
@@ -21,42 +20,46 @@ import IC.Debug.JSON ()
 
 startApp :: IO Application
 startApp = do
-    stateVar <- newMVar initialIC
+    stateVar <- newMVar [initialIC]
     return $ handle stateVar
 
-handle :: MVar IC -> Application
-handle stateVar req respond =
-        case (requestMethod req, pathInfo req) of
-            ("GET", []) -> do
-                withIC $ \ic ->
-                    json status200 ic
-            ("GET", ["api","v1","status"]) ->
-                cbor status200 IC.HTTP.Status.r
-            ("POST", ["api","v1","submit"]) ->
-                withCBOR $ \gr ->
-                    case IC.HTTP.Request.asyncRequest gr of
-                        Left err -> invalidRequest err
-                        Right ar -> do
-                            runIC $ do
-                                submitRequest (requestId gr) ar
-                                -- We could do processing separately
-                                -- (e.g. a separte worker threat)
-                                runToCompletion
-                            cbor status200 emptyR
-            ("POST", ["api","v1","read"]) ->
-                withCBOR $ \gr ->
-                    case IC.HTTP.Request.syncRequest gr of
-                        Left err -> invalidRequest err
-                        Right ar -> do
-                            r <- runIC $ readRequest ar
-                            cbor status200 (IC.HTTP.Request.response r)
-            _ -> notFound
+handle :: MVar [IC] -> Application
+handle stateVar req respond = case (requestMethod req, pathInfo req) of
+    ("GET", []) -> withHistory $ json status200
+    ("GET", ["api","v1","status"]) ->
+        cbor status200 IC.HTTP.Status.r
+    ("POST", ["api","v1","submit"]) ->
+        withCBOR $ \gr -> case IC.HTTP.Request.asyncRequest gr of
+            Left err -> invalidRequest err
+            Right ar -> do
+                runIC $ do
+                    -- enqueue request
+                    submitRequest (requestId gr) ar
+                    -- We could do the processing separately
+                    -- (e.g. a separte worker thread)
+                    -- also to show individual steps
+                    runToCompletion
+                cbor status200 emptyR
+    ("POST", ["api","v1","read"]) ->
+        withCBOR $ \gr -> case IC.HTTP.Request.syncRequest gr of
+            Left err -> invalidRequest err
+            Right ar -> do
+                r <- peekIC $ readRequest ar
+                cbor status200 (IC.HTTP.Request.response r)
+    _ -> notFound
   where
     runIC :: StateT IC IO a -> IO a
-    runIC a = modifyMVar stateVar (fmap swap . runStateT a)
+    runIC a = modifyMVar stateVar $ \(s:ss) -> do
+        (x, s') <- runStateT a s
+        return (s':s:ss, x)
 
-    withIC :: (IC -> IO a) -> IO a
-    withIC a = readMVar stateVar >>= a
+    peekIC :: StateT IC IO a -> IO a
+    peekIC a = modifyMVar stateVar $ \(s:ss) -> do
+        (x, _s') <- runStateT a s
+        return (s:ss, x)
+
+    withHistory :: ([IC] -> IO a) -> IO a
+    withHistory a = readMVar stateVar >>= a . reverse
 
     cbor status gr = respond $ responseBuilder
         status
@@ -73,12 +76,11 @@ handle stateVar req respond =
         respond $ responseBuilder status400 [] (T.encodeUtf8Builder msg)
     notFound = respond $ responseLBS status404 [] "Not found\n"
 
-    withCBOR k =
-        case lookup hContentType (requestHeaders req) of
-            Just "application/cbor" -> do
-                body <- strictRequestBody req
-                case IC.HTTP.CBOR.decode body of
-                    Left err -> invalidRequest err
-                    Right gr -> k gr
-            _ -> invalidRequest "Expected application/cbor request"
+    withCBOR k = case lookup hContentType (requestHeaders req) of
+        Just "application/cbor" -> do
+            body <- strictRequestBody req
+            case IC.HTTP.CBOR.decode body of
+                Left err -> invalidRequest err
+                Right gr -> k gr
+        _ -> invalidRequest "Expected application/cbor request"
 
