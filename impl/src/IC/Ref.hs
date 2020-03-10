75912b99dc630d1ea910048464513eb01d1f33ce
Joachim Breitner <mail@joachim-breitner.de>
2020-03-10 09:30:12 +0100

    Commenting and reordering pass through IC.Ref

--- impl/src/IC/Ref.hs
+++ impl/src/IC/Ref.hs
@@ -7,7 +7,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
 
-{-# OPTIONS_GHC -Wmissing-signatures #-}
+{-|
+This module implements the main abstract logic of the Internet Computer. It
+assumes a pure and abstracted view on Canisters (provided by "IC.Canister"),
+and deals with abstract requests ('AsyncRequest', 'SyncRequest'), so HTTP and
+CBOR-level processing has already happened.
+-}
 module IC.Ref
   ( IC(..)
   , AsyncRequest(..)
@@ -16,7 +21,8 @@ module IC.Ref
   , CompletionValue(..)
   , ReqResponse
   , initialIC
-  , submitRequest, readRequest
+  , submitRequest
+  , readRequest
   , runStep
   , runToCompletion
   -- $ Exported merely for introspection
@@ -34,11 +40,10 @@ import Control.Monad.State.Class
 import Data.Sequence (Seq(..))
 import Data.Foldable (toList)
 
--- import Debug.Trace
-
 import IC.Types
 import IC.Canister
 import IC.Id.Fresh
+import IC.Utils
 import IC.Logger
 
 -- Abstract HTTP Interface
@@ -55,7 +60,7 @@ data SyncRequest
     | StatusRequest Blob
 
 data RequestStatus
-  = Unknown -- never inside IC, only as ReqResponse
+  = Unknown -- never used inside the IC, only as ReqResponse
   | Received
   | Processing
   | Rejected (RejectCode, String)
@@ -64,37 +69,35 @@ data RequestStatus
 
 type ReqResponse = RequestStatus
 
-
-data CompletionValue -- we need to be more typed than the public spec here
+data CompletionValue -- ^ we need to be more typed than the public spec here
   = CompleteUnit
   | CompleteCanisterId CanisterId
   | CompleteArg Blob
   deriving (Show)
 
--- Abstract canisters
-
 -- IC state
 
+-- Canisters have a module (static) and state (dynamic)
 data CanState = CanState
   { wasm_state :: WasmState
   , can_mod :: CanisterModule
   }
   deriving (Show)
 
-type ICT m = (MonadState IC m, Logger m)
-
+-- A canister entry point is either a publicly named function, or a closure
+-- (callback + environment)
 data EntryPoint
   = Public MethodName Blob
   | Closure Callback Response
   deriving (Show)
 
 type CallId = Int
-
 data CallContext = CallContext
   { canister :: CanisterId
   , origin :: CallOrigin
   , responded :: Responded
   , last_trap :: Maybe String
+      -- ^ non-normative, but yields better reject messages
   }
   deriving (Show)
 
@@ -104,8 +107,8 @@ data CallOrigin
   | FromInit EntityId
   deriving (Show)
 
-data Message =
-  CallMessage
+data Message
+  = CallMessage
     { call_context :: CallId
     , entry :: EntryPoint
     }
@@ -115,6 +118,8 @@ data Message =
     }
   deriving (Show)
 
+-- Finally, the full IC state:
+
 data IC = IC
   { canisters :: CanisterId ↦ Maybe CanState
   , requests :: RequestID ↦ (AsyncRequest, RequestStatus)
@@ -123,18 +128,48 @@ data IC = IC
   }
   deriving (Show)
 
+-- The functions below want stateful access to a value of type 'IC', and be
+-- able to log message (used for `ic0.debug_print`).
+type ICM m = (MonadState IC m, Logger m)
+
 initialIC :: IC
 initialIC = IC mempty mempty mempty mempty
 
-submitRequest :: ICT m => RequestID -> AsyncRequest -> m ()
-submitRequest rid r = modify $ \ic -> ic
-    { requests = M.insert rid (r, Received) (requests ic)
-    }
+-- Request handling
 
 findRequest :: RequestID -> IC -> Maybe (AsyncRequest, RequestStatus)
 findRequest rid ic = M.lookup rid (requests ic)
 
-readRequest :: ICT m => SyncRequest -> m ReqResponse
+setReqStatus :: ICM m => RequestID -> RequestStatus -> m ()
+setReqStatus rid s = modify $ \ic ->
+  ic { requests = M.adjust (\(r,_) -> (r,s)) rid (requests ic) }
+
+callerOfRequest :: ICM m => RequestID -> m EntityId
+callerOfRequest rid = gets (M.lookup rid . requests) >>= \case
+    Just (CreateRequest user_id _, _) -> return user_id
+    Just (InstallRequest _ user_id _ _ _, _) -> return user_id
+    Just (UpgradeRequest _ user_id _ _, _) -> return user_id
+    Just (UpdateRequest _ user_id _ _, _) -> return user_id
+    Nothing -> error "callerOfRequest"
+
+
+-- Canister handling
+
+createEmptyCanister :: ICM m => CanisterId -> m ()
+createEmptyCanister cid = modify $ \ic ->
+  ic { canisters = M.insert cid Nothing (canisters ic) }
+
+insertCanister :: ICM m => CanisterId -> CanisterModule -> WasmState -> m ()
+insertCanister cid can_mod wasm_state = modify $ \ic ->
+  ic { canisters = M.insert cid (Just (CanState {can_mod, wasm_state})) (canisters ic) }
+
+setCanisterState :: ICM m => CanisterId -> WasmState -> m ()
+setCanisterState cid wasm_state = modify $ \ic ->
+  ic { canisters = M.adjust (fmap (\cs -> cs { wasm_state })) cid (canisters ic) }
+
+-- Synchronous requests
+
+readRequest :: ICM m => SyncRequest -> m ReqResponse
 
 readRequest (StatusRequest rid) =
   gets (findRequest rid) >>= \case
@@ -154,55 +189,18 @@ readRequest (QueryRequest canister_id user_id method arg) =
             Return (Reply res) -> return $ Completed (CompleteArg res)
             Return (Reject (rc,rm)) -> return $ Rejected (rc, rm)
 
-nextReceived :: ICT m => m (Maybe (RequestID, AsyncRequest))
-nextReceived = gets $ \ic -> listToMaybe
-  [ (rid,r) | (rid, (r, Received)) <- M.toList (requests ic) ]
+-- Asynchronous requests
 
-nextStarved :: ICT m => m (Maybe CallId)
-nextStarved = gets $ \ic -> listToMaybe
-  [ c
-  | (c, ctxt) <- M.toList (call_contexts ic)
-  , Responded False <- return $ responded ctxt
-  , null [ () | ResponseMessage { call_context = c' } <- toList (messages ic), c' == c ]
-  , null
-      [ ()
-      | CallContext { responded = Responded False, origin = FromCanister c' _}
-          <- M.elems (call_contexts ic)
-      , c' == c
-      ]
-  ]
+-- | Submission simply enqueues requests
 
-nextMessage :: ICT m => m (Maybe Message)
-nextMessage = state $ \ic ->
-  case messages ic of
-    Empty -> (Nothing, ic)
-    m :<| ms -> (Just m, ic { messages = ms })
-
-setReqStatus :: ICT m => RequestID -> RequestStatus -> m ()
-setReqStatus rid s =
-  modify (\ic -> ic { requests = M.adjust (\(r,_) -> (r,s)) rid (requests ic) })
-
-createEmptyCanister :: ICT m => CanisterId -> m ()
-createEmptyCanister cid =
-  modify (\ic -> ic { canisters =
-    M.insert cid Nothing (canisters ic)
-  })
+submitRequest :: ICM m => RequestID -> AsyncRequest -> m ()
+submitRequest rid r = modify $ \ic ->
+  ic { requests = M.insert rid (r, Received) (requests ic) }
 
 
-installCanister :: ICT m => CanisterId -> CanisterModule -> WasmState -> m ()
-installCanister cid can_mod wasm_state =
-  -- Check that canister exists but is empty before? No, also used for upgrade.
-  modify (\ic -> ic { canisters =
-    M.insert cid (Just (CanState {can_mod, wasm_state})) (canisters ic)
-  })
+-- | Eventually, they are processed
 
-setCanisterState :: ICT m => CanisterId -> WasmState -> m ()
-setCanisterState cid wasm_state =
-  modify (\ic -> ic { canisters =
-    M.adjust (fmap (\cs -> cs { wasm_state })) cid (canisters ic)
-  })
-
-processRequest :: ICT m => RequestID -> AsyncRequest -> m ()
+processRequest :: ICM m => RequestID -> AsyncRequest -> m ()
 
 processRequest rid (CreateRequest _user_id (Just desired)) = do
   let res = setReqStatus rid
@@ -229,13 +227,12 @@ processRequest rid (InstallRequest canister_id user_id can_mod dat reinstall) = 
         Just old_canister -> case (reinstall, isJust old_canister) of
           (False, True) -> res $ Rejected (RC_DESTINATION_INVALID, "canister is not empty during installation")
           (True, False) -> res $ Rejected (RC_DESTINATION_INVALID, "canister is empty during reinstallation")
-          _ -> do
-            case init_method can_mod canister_id user_id dat of
-              Trap msg ->
-                res $ Rejected (RC_CANISTER_ERROR, "Initialization trapped: " ++ msg)
-              Return wasm_state -> do
-                installCanister canister_id can_mod wasm_state
-                res $ Completed CompleteUnit
+          _ -> case init_method can_mod canister_id user_id dat of
+            Trap msg ->
+              res $ Rejected (RC_CANISTER_ERROR, "Initialization trapped: " ++ msg)
+            Return wasm_state -> do
+              insertCanister canister_id can_mod wasm_state
+              res $ Completed CompleteUnit
 
 processRequest rid (UpgradeRequest canister_id user_id new_can_mod dat) = do
   let res = setReqStatus rid
@@ -252,7 +249,7 @@ processRequest rid (UpgradeRequest canister_id user_id new_can_mod dat) = do
               case post_upgrade_method new_can_mod user_id user_id mem dat of
                 Trap msg -> res $ Rejected (RC_CANISTER_ERROR, "post-upgrade trapped: " ++ msg)
                 Return new_wasm_state -> do
-                  installCanister canister_id new_can_mod new_wasm_state
+                  insertCanister canister_id new_can_mod new_wasm_state
                   res $ Completed CompleteUnit
 
 processRequest rid (UpdateRequest canister_id _user_id method arg) = do
@@ -268,44 +265,31 @@ processRequest rid (UpdateRequest canister_id _user_id method arg) = do
     }
   setReqStatus rid Processing
 
-enqueueMessage :: ICT m => Message -> m ()
-enqueueMessage m = modify $ \ic -> ic { messages = messages ic :|> m }
+-- Call context handling
 
-newCallContext :: ICT m => CallContext -> m CallId
-newCallContext cc = state go
-  where
-    go ic  = (i, ic { call_contexts = M.insert i cc (call_contexts ic)})
-      where
-        i | M.null (call_contexts ic) = 0
-          | otherwise = fst (M.findMax (call_contexts ic)) + 1
+newCallContext :: ICM m => CallContext -> m CallId
+newCallContext cc = state $ \ic ->
+  let i = freshKey (call_contexts ic)
+  in (i, ic { call_contexts = M.insert i cc (call_contexts ic)})
 
-getCallContext :: ICT m => CallId -> m CallContext
-getCallContext ctxt_id =
-  gets ((M.! ctxt_id) . call_contexts)
+getCallContext :: ICM m => CallId -> m CallContext
+getCallContext ctxt_id = gets ((M.! ctxt_id) . call_contexts)
 
-modifyCallContext :: ICT m => CallId -> (CallContext -> CallContext) -> m ()
-modifyCallContext ctxt_id f =
-  modify $ \ic -> ic { call_contexts = M.adjust f ctxt_id (call_contexts ic) }
+modifyCallContext :: ICM m => CallId -> (CallContext -> CallContext) -> m ()
+modifyCallContext ctxt_id f = modify $ \ic ->
+  ic { call_contexts = M.adjust f ctxt_id (call_contexts ic) }
 
-respondCallContext :: ICT m => CallId -> Response -> m ()
+respondCallContext :: ICM m => CallId -> Response -> m ()
 respondCallContext ctxt_id response = do
   -- TODO: check no prior response
   modifyCallContext ctxt_id $ \ctxt -> ctxt { responded = Responded True }
   enqueueMessage $ ResponseMessage { call_context = ctxt_id, response }
 
-rememberTrap :: ICT m => CallId -> String -> m ()
+rememberTrap :: ICM m => CallId -> String -> m ()
 rememberTrap ctxt_id msg =
   modifyCallContext ctxt_id $ \ctxt -> ctxt { last_trap = Just msg }
 
-callerOfRequest :: ICT m => RequestID -> m EntityId
-callerOfRequest rid = gets (M.lookup rid . requests) >>= \case
-    Just (CreateRequest user_id _, _) -> return user_id
-    Just (InstallRequest _ user_id _ _ _, _) -> return user_id
-    Just (UpgradeRequest _ user_id _ _, _) -> return user_id
-    Just (UpdateRequest _ user_id _ _, _) -> return user_id
-    Nothing -> error "callerOfRequest"
-
-callerOfCallID :: ICT m => CallId -> m EntityId
+callerOfCallID :: ICM m => CallId -> m EntityId
 callerOfCallID ctxt_id = do
   ctxt <- getCallContext ctxt_id
   case origin ctxt of
@@ -313,43 +297,25 @@ callerOfCallID ctxt_id = do
     FromCanister other_ctxt_id _callback -> calleeOfCallID other_ctxt_id
     FromInit entity_id -> return entity_id
 
-calleeOfCallID :: ICT m => CallId -> m EntityId
+calleeOfCallID :: ICM m => CallId -> m EntityId
 calleeOfCallID ctxt_id = canister <$> getCallContext ctxt_id
 
-respondedCallID :: ICT m => CallId -> m Responded
+respondedCallID :: ICM m => CallId -> m Responded
 respondedCallID ctxt_id = responded <$> getCallContext ctxt_id
 
-invokeEntry :: ICT m =>
-    CallId -> CanState -> EntryPoint ->
-    m (TrapOr (WasmState, UpdateResult))
-invokeEntry ctxt_id (CanState wasm_state can_mod) entry = do
-    responded <- respondedCallID ctxt_id
-    case entry of
-      Public method dat -> do
-        caller <- callerOfCallID ctxt_id
-        case M.lookup method (update_methods can_mod) of
-          Just f ->
-            return $ f caller responded dat wasm_state
-          Nothing -> do
-            let reject = Reject (RC_DESTINATION_INVALID, "update method does not exist: " ++ method)
-            return $ Return (wasm_state, ([], Just reject))
-      Closure cb r ->
-        return $ callbacks can_mod cb responded r wasm_state
+starveCallContext :: ICM m => CallId -> m ()
+starveCallContext ctxt_id = do
+  ctxt <- getCallContext ctxt_id
+  let msg | Just t <- last_trap ctxt = "canister trapped: " ++ t
+          | otherwise                = "canister did not respond"
+  respondCallContext ctxt_id $ Reject (RC_CANISTER_ERROR, msg)
 
-newCall :: ICT m => CallId -> MethodCall -> m ()
-newCall from_ctxt_id call = do
-  new_ctxt_id <- newCallContext $ CallContext
-    { canister = call_callee call
-    , origin = FromCanister from_ctxt_id (call_callback call)
-    , responded = Responded False
-    , last_trap = Nothing
-    }
-  enqueueMessage $ CallMessage
-    { call_context = new_ctxt_id
-    , entry = Public (call_method_name call) (call_arg call)
-    }
+-- Message handling
 
-processMessage :: ICT m => Message -> m ()
+enqueueMessage :: ICM m => Message -> m ()
+enqueueMessage m = modify $ \ic -> ic { messages = messages ic :|> m }
+
+processMessage :: ICM m => Message -> m ()
 processMessage (CallMessage ctxt_id entry) = do
   callee <- calleeOfCallID ctxt_id
   let res r = respondCallContext ctxt_id r
@@ -380,26 +346,77 @@ processMessage (ResponseMessage ctxt_id response) = do
         }
     FromInit _ -> error "unexpected Response in Init"
 
-starveCallContext :: ICT m => CallId -> m ()
-starveCallContext ctxt_id = do
-  ctxt <- getCallContext ctxt_id
-  let msg | Just t <- last_trap ctxt = "canister trapped: " ++ t
-          | otherwise                = "canister did not respond"
-  respondCallContext ctxt_id $ Reject (RC_CANISTER_ERROR, msg)
+invokeEntry :: ICM m =>
+    CallId -> CanState -> EntryPoint ->
+    m (TrapOr (WasmState, UpdateResult))
+invokeEntry ctxt_id (CanState wasm_state can_mod) entry = do
+    responded <- respondedCallID ctxt_id
+    case entry of
+      Public method dat -> do
+        caller <- callerOfCallID ctxt_id
+        case M.lookup method (update_methods can_mod) of
+          Just f ->
+            return $ f caller responded dat wasm_state
+          Nothing -> do
+            let reject = Reject (RC_DESTINATION_INVALID, "update method does not exist: " ++ method)
+            return $ Return (wasm_state, ([], Just reject))
+      Closure cb r ->
+        return $ callbacks can_mod cb responded r wasm_state
+
+newCall :: ICM m => CallId -> MethodCall -> m ()
+newCall from_ctxt_id call = do
+  new_ctxt_id <- newCallContext $ CallContext
+    { canister = call_callee call
+    , origin = FromCanister from_ctxt_id (call_callback call)
+    , responded = Responded False
+    , last_trap = Nothing
+    }
+  enqueueMessage $ CallMessage
+    { call_context = new_ctxt_id
+    , entry = Public (call_method_name call) (call_arg call)
+    }
+
+-- Scheduling
+
+-- | Pick next request in state `received`
+nextReceived :: ICM m => m (Maybe (RequestID, AsyncRequest))
+nextReceived = gets $ \ic -> listToMaybe
+  [ (rid,r) | (rid, (r, Received)) <- M.toList (requests ic) ]
+
+-- | Find a starved call context
+nextStarved :: ICM m => m (Maybe CallId)
+nextStarved = gets $ \ic -> listToMaybe
+  [ c
+  | (c, ctxt) <- M.toList (call_contexts ic)
+  , Responded False <- return $ responded ctxt
+  , null [ ()
+      | ResponseMessage { call_context = c' } <- toList (messages ic)
+      , c' == c ]
+  , null [ ()
+      | CallContext { responded = Responded False, origin = FromCanister c' _}
+          <- M.elems (call_contexts ic)
+      , c' == c
+      ]
+  ]
+
+-- | Pick (and remove) next message from queue
+popMessage :: ICM m => m (Maybe Message)
+popMessage = state $ \ic ->
+  case messages ic of
+    Empty -> (Nothing, ic)
+    m :<| ms -> (Just m, ic { messages = ms })
+
 
 -- | Returns true if a step was taken
-runStep :: ICT m => m Bool
+runStep :: ICM m => m Bool
 runStep =
   nextReceived >>= \case
     Just (rid,r) -> processRequest rid r >> return True
-    Nothing -> nextMessage >>= \case
+    Nothing -> popMessage >>= \case
       Just m  -> processMessage m >> return True
       Nothing -> nextStarved >>= \case
         Just c  -> starveCallContext c >> return True
         Nothing -> return False
 
-runToCompletion :: ICT m => m ()
-runToCompletion =
-  runStep >>= \case
-    True -> runToCompletion
-    False -> return ()
+runToCompletion :: ICM m => m ()
+runToCompletion = repeatWhileTrue runStep
