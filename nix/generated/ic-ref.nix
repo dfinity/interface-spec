91f96ee9dcaaf69a7d9c2c2d816c2894b4181f9a
Joachim Breitner <mail@joachim-breitner.de>
2020-05-06 12:27:41 +0200

    Post-release commit

--- nix/generated/ic-ref.nix
+++ nix/generated/ic-ref.nix
@@ -10,7 +10,7 @@
 }:
 mkDerivation {
   pname = "ic-ref";
-  version = "0.2.12";
+  version = "0.2.13";
   src = import ../gitSource.nix "impl";
   configureFlags = [ "-frelease" ];
   isLibrary = false;
