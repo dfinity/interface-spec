2defae77fb1ddde6d22fe2b49555e61d1d90b2f1
Joachim Breitner <mail@joachim-breitner.de>
2020-05-29 15:12:08 +0200

    Release 0.2.16

--- nix/generated/ic-ref.nix
+++ nix/generated/ic-ref.nix
@@ -10,7 +10,7 @@
 }:
 mkDerivation {
   pname = "ic-ref";
-  version = "0.2.15";
+  version = "0.2.16";
   src = import ../gitSource.nix "impl";
   configureFlags = [ "-frelease" ];
   isLibrary = false;
