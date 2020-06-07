b8399b07d61926c9ceddf8c1388014c599c3d85f
Joachim Breitner <mail@joachim-breitner.de>
2020-06-07 18:54:49 +0200

    Github commenter: Remove spaces that cause repeated commenting
    see
    https://dfinity.slack.com/archives/CL7Q2RXUM/p1591548471088900?thread_ts=1591478202.086400&cid=CL7Q2RXUM
    for an analysis

--- default.nix
+++ default.nix
@@ -128,8 +128,8 @@ rec {
 
       comment_template=$out/nix-support/gh-comment
       cat <<EOF >$comment_template
-        {{{! comment:edit-one }}}This PR changes the public spec, you can preview the result at:
-        {{{ host }}}/latest/{{{ project }}}/{{{ jobset }}}/{{{ job }}}/1/index.html
+      {{{! comment:edit-one }}}This PR changes the public spec, you can preview the result at:
+      {{{ host }}}/latest/{{{ project }}}/{{{ jobset }}}/{{{ job }}}/1/index.html
       EOF
       echo "comment manifest $comment_template comment.mustache" >> $out/nix-support/hydra-build-products
 
