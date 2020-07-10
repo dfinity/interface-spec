2b9f358d2deee95d9ebba08f2ce6f428b1a3e51a
Joachim Breitner <mail@joachim-breitner.de>
2020-07-10 15:23:06 +0200

    ic-ref: remove obsolete comment

--- impl/src/IC/Canister/Interface.hs
+++ impl/src/IC/Canister/Interface.hs
@@ -5,15 +5,12 @@ This module defines the various entry points provided by "IC.Canister.Imp" as a 
 
 The point of this abstraction is to to allow adding new entry points, or changing the arguments or result values, without changing the modules "IC.Canister.Pure" or "IC.Canister.Persisted", that sit between "IC.Canister.Imp" and "IC.Canister".
 
-Certain features here are beyond the specified abilities, in order to support the Motoko test suite. In particular:
-
- * Issuing calls from canister_init
-
 -}
 module IC.Canister.Interface where
 
 import IC.Types
 import IC.Wasm.Winter (Module)
+
 
 data CanisterMethod r where
     Initialize :: Module -> EntityId -> Blob -> CanisterMethod ()
