37468552ba30e6052e6624cfde432e10794378f5
Joachim Breitner <mail@joachim-breitner.de>
2020-08-05 17:06:30 +0200

    Bump Candid (better error messages)

--- nix/generated/candid.nix
+++ nix/generated/candid.nix
@@ -1,12 +1,12 @@
 # THIS IS AN AUTOMATICALLY GENERATED FILE. DO NOT EDIT MANUALLY!
 # See ./nix/generate.nix for instructions.
 
-{ mkDerivation, pkgs, base, base32, bytestring, cereal, containers, crc
-, dlist, doctest, hex-text, leb128-cereal, megaparsec, mtl
-, optparse-applicative, prettyprinter, row-types, scientific
-, smallcheck, split, stdenv, tasty, tasty-hunit, tasty-rerun
-, tasty-smallcheck, template-haskell, text, unordered-containers
-, vector
+{ mkDerivation, pkgs, base, base32, bytestring, cereal, constraints
+, containers, crc, dlist, doctest, hex-text, leb128-cereal
+, megaparsec, mtl, optparse-applicative, prettyprinter, row-types
+, scientific, smallcheck, split, stdenv, tasty, tasty-hunit
+, tasty-rerun, tasty-smallcheck, template-haskell, text
+, unordered-containers, vector
 }:
 mkDerivation {
   pname = "candid";
@@ -15,9 +15,9 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base32 bytestring cereal containers crc dlist hex-text
-    leb128-cereal megaparsec mtl prettyprinter row-types scientific
-    split template-haskell text unordered-containers vector
+    base base32 bytestring cereal constraints containers crc dlist
+    hex-text leb128-cereal megaparsec mtl prettyprinter row-types
+    scientific split template-haskell text unordered-containers vector
   ];
   executableHaskellDepends = [
     base bytestring hex-text optparse-applicative prettyprinter text
