107492421cde47319e10e6feeb0745b688487736
Joachim Breitner <mail@joachim-breitner.de>
2020-08-06 15:23:30 +0200

    Refine nix/generate.nix
    copying back my improvements in dfinity/hs.

--- nix/generated/candid.nix
+++ nix/generated/candid.nix
@@ -1,12 +1,36 @@
 # THIS IS AN AUTOMATICALLY GENERATED FILE. DO NOT EDIT MANUALLY!
 # See ./nix/generate.nix for instructions.
 
-{ mkDerivation, pkgs, base, base32, bytestring, cereal, constraints
-, containers, crc, dlist, doctest, hex-text, leb128-cereal
-, megaparsec, mtl, optparse-applicative, prettyprinter, row-types
-, scientific, smallcheck, split, stdenv, tasty, tasty-hunit
-, tasty-rerun, tasty-smallcheck, template-haskell, text
-, unordered-containers, vector
+{ mkDerivation
+, pkgs
+, base
+, base32
+, bytestring
+, cereal
+, constraints
+, containers
+, crc
+, dlist
+, doctest
+, hex-text
+, leb128-cereal
+, megaparsec
+, mtl
+, optparse-applicative
+, prettyprinter
+, row-types
+, scientific
+, smallcheck
+, split
+, stdenv
+, tasty
+, tasty-hunit
+, tasty-rerun
+, tasty-smallcheck
+, template-haskell
+, text
+, unordered-containers
+, vector
 }:
 mkDerivation {
   pname = "candid";
@@ -15,17 +39,50 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base32 bytestring cereal constraints containers crc dlist
-    hex-text leb128-cereal megaparsec mtl prettyprinter row-types
-    scientific split template-haskell text unordered-containers vector
+    base
+    base32
+    bytestring
+    cereal
+    constraints
+    containers
+    crc
+    dlist
+    hex-text
+    leb128-cereal
+    megaparsec
+    mtl
+    prettyprinter
+    row-types
+    scientific
+    split
+    template-haskell
+    text
+    unordered-containers
+    vector
   ];
   executableHaskellDepends = [
-    base bytestring hex-text optparse-applicative prettyprinter text
+    base
+    bytestring
+    hex-text
+    optparse-applicative
+    prettyprinter
+    text
   ];
   testHaskellDepends = [
-    base bytestring doctest leb128-cereal prettyprinter row-types
-    smallcheck tasty tasty-hunit tasty-rerun tasty-smallcheck text
-    unordered-containers vector
+    base
+    bytestring
+    doctest
+    leb128-cereal
+    prettyprinter
+    row-types
+    smallcheck
+    tasty
+    tasty-hunit
+    tasty-rerun
+    tasty-smallcheck
+    text
+    unordered-containers
+    vector
   ];
   license = stdenv.lib.licenses.asl20;
 }
