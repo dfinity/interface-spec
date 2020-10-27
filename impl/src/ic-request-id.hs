2575eedfd6fdd29a11f8cb806549b49de2b9f566
Joachim Breitner <mail@joachim-breitner.de>
2020-10-27 11:26:30 +0100

    The ic-request-id CLI tool now expects a request _with_ envelope

--- impl/src/ic-request-id.hs
+++ impl/src/ic-request-id.hs
@@ -4,11 +4,15 @@ module Main where
 import Options.Applicative
 import Control.Monad (join)
 import qualified Data.ByteString.Lazy as BS
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Text.Hex as H
+import qualified Data.HashMap.Lazy as HM
 import System.IO
 
+
 import IC.HTTP.CBOR
+import IC.HTTP.GenR
 import IC.HTTP.RequestId
 
 work :: Maybe FilePath -> IO ()
@@ -18,15 +22,18 @@ work input = do
     Left err -> do
         T.hPutStrLn stderr "Failed to decode CBOR:"
         T.hPutStrLn stderr err
-    Right gr ->
-        T.putStrLn $ H.encodeHex $ BS.toStrict $ requestId gr
+    Right (GRec m) | Just content <- HM.lookup "content" m ->
+        T.putStrLn $ H.encodeHex $ BS.toStrict $ requestId content
+    Right gr -> do
+        T.hPutStrLn stderr "Request does not look like an envelop (could not find field \"content\"):"
+        T.hPutStrLn stderr (T.pack (show gr))
 
 main :: IO ()
 main = join . customExecParser (prefs showHelpOnError) $
   info (helper <*> parser)
   (  fullDesc
   <> header "Internet Computer request id"
-  <> progDesc "Given a CBOR-encoded request, calculate the request id"
+  <> progDesc "Given a CBOR-encoded request with envelope, calculate the request id"
   )
   where
     parser :: Parser (IO ())
