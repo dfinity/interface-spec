0b04c9a295a859f45f1b40e48207cc5837af44e7
Joachim Breitner <mail@joachim-breitner.de>
2020-11-04 22:27:23 +0100

    nix: set preferLocalBuild = true in gitSource

--- nix/gitSource.nix
+++ nix/gitSource.nix
@@ -77,7 +77,7 @@ then
           else abort "gitSource.nix: Cannot parse ${toString ./../.git}";
 
     whitelist_file =
-      nixpkgs.runCommand "git-ls-files" {envVariable = true;} ''
+      nixpkgs.runCommand "git-ls-files" {envVariable = true; preferLocalBuild = true;} ''
         cp ${git_dir + "/index"} index
         echo "ref: refs/heads/master" > HEAD
         mkdir objects refs
