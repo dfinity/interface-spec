bb99fb37cf67f2f42c8d541003ad7051cf1675ca
Joachim Breitner <mail@joachim-breitner.de>
2020-12-17 18:22:42 +0100

    ic-ref: Remove support for raw ed25519 keys

--- impl/src/IC/Crypto.hs
+++ impl/src/IC/Crypto.hs
@@ -6,7 +6,6 @@ Everything related to signature creation and checking
 module IC.Crypto
  ( SecretKey
  , createSecretKeyEd25519
- , createSecretKeyEd25519Raw
  , createSecretKeyWebAuthn
  , createSecretKeyECDSA
  , createSecretKeyBLS
@@ -25,12 +24,10 @@ import qualified IC.Crypto.WebAuthn as WebAuthn
 import qualified IC.Crypto.ECDSA as ECDSA
 import qualified IC.Crypto.BLS as BLS
 import Data.Int
-import Data.Bifunctor
 import Control.Monad.Except
 
 data SecretKey
     = Ed25519 Ed25519.SecretKey
-    | Ed25519Raw Ed25519.SecretKey -- non-DER encoded public keys
     | ECDSA ECDSA.SecretKey
     | WebAuthn WebAuthn.SecretKey
     | BLS BLS.SecretKey
@@ -38,9 +35,6 @@ data SecretKey
 
 createSecretKeyEd25519 :: BS.ByteString -> SecretKey
 createSecretKeyEd25519 = Ed25519 . Ed25519.createKey
-
-createSecretKeyEd25519Raw :: BS.ByteString -> SecretKey
-createSecretKeyEd25519Raw = Ed25519Raw . Ed25519.createKey
 
 createSecretKeyWebAuthn :: BS.ByteString -> SecretKey
 createSecretKeyWebAuthn = WebAuthn . WebAuthn.createKey
@@ -52,7 +46,6 @@ createSecretKeyBLS :: BS.ByteString -> SecretKey
 createSecretKeyBLS = BLS . BLS.createKey
 
 toPublicKey :: SecretKey -> BS.ByteString
-toPublicKey (Ed25519Raw sk) = Ed25519.toPublicKey sk
 toPublicKey (Ed25519 sk) = DER.encode DER.Ed25519 $ Ed25519.toPublicKey sk
 toPublicKey (WebAuthn sk) = DER.encode DER.WebAuthn $ WebAuthn.toPublicKey sk
 toPublicKey (ECDSA sk) = DER.encode DER.ECDSA $ ECDSA.toPublicKey sk
@@ -61,7 +54,6 @@ toPublicKey (BLS sk) = DER.encode DER.BLS $ BLS.toPublicKey sk
 signPure :: BS.ByteString -> SecretKey -> BS.ByteString -> BS.ByteString
 signPure domain_sep sk payload = case sk of
     Ed25519 sk -> Ed25519.sign sk msg
-    Ed25519Raw sk -> Ed25519.sign sk msg
     WebAuthn _ -> error "WebAuthn not a pure signature"
     ECDSA _ -> error "ECDSA not a pure signature"
     BLS sk -> BLS.sign sk msg
@@ -72,7 +64,6 @@ signPure domain_sep sk payload = case sk of
 sign :: BS.ByteString -> SecretKey -> BS.ByteString -> IO BS.ByteString
 sign domain_sep sk payload = case sk of
     Ed25519 sk -> return $ Ed25519.sign sk msg
-    Ed25519Raw sk -> return $ Ed25519.sign sk msg
     WebAuthn sk -> WebAuthn.sign sk msg
     ECDSA sk -> ECDSA.sign sk msg
     BLS sk -> return $ BLS.sign sk msg
@@ -80,12 +71,8 @@ sign domain_sep sk payload = case sk of
     msg | BS.null domain_sep = payload
         | otherwise = BS.singleton (fromIntegral (BS.length domain_sep)) <> domain_sep <> payload
 
-unpack :: BS.ByteString -> Either T.Text (DER.Suite, BS.ByteString)
-unpack pk | BS.length pk == 32 = Right (DER.Ed25519, pk) -- raw format
-          | otherwise          = first T.pack $ DER.decode pk
-
 verify :: BS.ByteString -> BS.ByteString -> BS.ByteString -> BS.ByteString -> Either T.Text ()
-verify domain_sep der_pk payload sig = unpack der_pk >>= \case
+verify domain_sep der_pk payload sig = DER.decode der_pk >>= \case
   (DER.WebAuthn, pk) -> do
     unless (WebAuthn.verify pk msg sig) $ do
         when (WebAuthn.verify pk payload sig) $
