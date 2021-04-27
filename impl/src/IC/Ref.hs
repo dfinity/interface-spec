d933c2dd468e394b66acd09b3d0d31a3f5bbae6f
Joachim Breitner <mail@joachim-breitner.de>
2021-04-27 17:39:46 +0200

    ic-ref: Let time progess more realistically (#333)
    fixes #332

--- impl/src/IC/Ref.hs
+++ impl/src/IC/Ref.hs
@@ -37,6 +37,7 @@ module IC.Ref
   , handleReadState
   , runStep
   , runToCompletion
+  , setAllTimesTo
   -- $ Exported merely for debug introspection
   , CallContext(..)
   , Message(..)
@@ -1056,6 +1057,10 @@ bumpTime :: ICM m => m ()
 bumpTime = modify $
   \ic -> ic { canisters = M.map (\cs -> cs { time = time cs +1 }) (canisters ic) }
 
+setAllTimesTo :: ICM m => Timestamp -> m ()
+setAllTimesTo ts = modify $
+  \ic -> ic { canisters = M.map (\cs -> cs { time = ts }) (canisters ic) }
+
 
 -- | Returns true if a step was taken
 runStep :: ICM m => m Bool
@@ -1073,6 +1078,8 @@ runStep = do
 
 runToCompletion :: ICM m => m ()
 runToCompletion = repeatWhileTrue runStep
+
+
 
 -- Error handling plumbing
 
